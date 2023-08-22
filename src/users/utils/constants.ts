79b930fe077ce4a78e9764ab73a28d2a13898a1e
b.fabry <b.fabry@groupeonepoint.com>
2023-08-22 18:41:36 +0200

    Add User Module

---
+++ src/users/utils/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * (?=.*[a-z])	The string must contain at least 1 lowercase alphabetical character
+ * (?=.*[A-Z])	The string must contain at least 1 uppercase alphabetical character
+ * (?=.*[\d])	The string must contain at least 1 numeric character
+ * (?=.*[\W_])	The string must contain at least one special character (adding _)
+ * (?=^.{12,30}$)	The string must be between 12 and 30 characters
+ *
+ * Inspired from : https://www.thepolyglotdeveloper.com/2015/05/use-regex-to-test-password-strength-in-javascript/
+ *
+ * RegExp cheatsheet : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+ */
+export const PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W_])(?=^.{12,30}$)/;
