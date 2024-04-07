b8772af410bd34d5199d9c720a565f9ce2fb8ba6
Benjamin FABRY <fabrybenjamin5@gmail.com>
2024-04-07 17:26:59 +0200

    format validation errors

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,9 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
+  @IsNotEmpty()
   email: string;
 
-  @IsString()
+  @IsNotEmpty()
   password: string;
 }
