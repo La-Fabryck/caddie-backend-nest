cc96b38f8f6cb705db38a552e157c59130538c09
befabry <fabrybenjamin5@gmail.com>
2025-03-18 16:52:41 +0100

    feat(authentication): add error messages

--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -1,9 +1,10 @@
 import { IsNotBlank } from '@/lib/decorators/is-not-blank';
+import { INVALID_EMAIL, INVALID_PASSWORD } from '../messages/authentication';
 
 export class LoginDto {
-  @IsNotBlank()
+  @IsNotBlank({ message: INVALID_EMAIL })
   email!: string;
 
-  @IsNotBlank()
+  @IsNotBlank({ message: INVALID_PASSWORD })
   password!: string;
 }
